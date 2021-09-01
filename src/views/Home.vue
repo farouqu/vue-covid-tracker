fdd48b9fe90d5fbd6aeacda66db71fd5cdae358e
Farouq Umar <ufakz99@gmail.com>
2021-09-01 01:39:42 +0100

    feat: implemented toggle country

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,10 @@
         <DataBoxes :stats="stats" />
 
         <CountrySelect v-on:get-country="getCountryData" :countries="countries" />
+
+        <button v-on:click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+            Clear Country
+        </button>
     </main>
     <main class="flex flex-col align-center justify-center text-center" v-else>
         <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -48,6 +52,13 @@ export default {
     getCountryData(country){
         this.stats = country
         this.title = country.Country
+    },
+    async clearCountryData(){
+        this.loading = true
+        const data = await this.fetchCovidData()
+        this.title = 'Global'
+        this.stats = data.Global
+        this.loading = false
     }
   },
   async created() {
