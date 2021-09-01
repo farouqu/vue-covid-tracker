ae31fcaa97364e232d8b47928538f4ed6abc0e48
Farouq Umar <ufakz99@gmail.com>
2021-09-01 01:11:58 +0100

    feat: added global cases and deaths count

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="home">
-      Hello World
+  <div>
+    <main v-if="!loading">
+        <DataTitle :text="title" :dataDate="dataDate" />
+
+        <DataBoxes :stats="stats" />
+    </main>
+    <main class="flex flex-col align-center justify-center text-center" v-else>
+        <div class="text-gray-500 text-3xl mt-10 mb-6">
+            Fetching Data
+        </div>
+        <img :src="loadingImage" alt="" class="w-24 m-auto">
+    </main>
   </div>
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    
-  }
-}
+      DataTitle,
+      DataBoxes
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.countries;
+    this.loading = false;
+  },
+};
 </script>
